d576301ab5d8235750d656de41f98b82f58f8ef7
Dagur Hardarson <201810188@post.au.dk>
2025-02-21 17:57:08 +0100

    restructure files

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,7 +5,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from fastapi.testclient import TestClient
-from main import app
+from src.main import app
 
 client = TestClient(app)
 
