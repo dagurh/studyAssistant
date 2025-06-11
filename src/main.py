ad87670bfcaf4a4746f256958e01da70df91fe11
Dagur Hardarson <201810188@post.au.dk>
2025-06-11 23:25:46 +0200

    allow frontend

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Or ["*"] for dev
+    allow_origins=["https://study-asssistant-front-end.onrender.com/"],  # Or ["*"] for dev
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
